9c5afa0786071201139e9a3757818dbb0707d9e5
Jake-Ranch <60143653+Jake-Ranch@users.noreply.github.com>
2025-06-11 14:51:49 +0800

    Add files via upload

---
+++ base_color.py
@@ -0,0 +1,8 @@
+brightness=2#亮度
+green = [brightness, 0, 0]#GRB
+red = [0, brightness, 0]
+blue = [0, 0, brightness]
+black = [0, 0, 0]  # 黑色
+white = [brightness, brightness, brightness]  # 白色
+grey = [brightness//2, brightness//2, brightness//2]  # 灰色
+
